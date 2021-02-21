a6c8b821c379d216bfb89ff92da603e595059850
samarskiy <samarskyii@gmail.com>
2021-02-21 14:25:52 +0300

    update for hw8

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,13 +17,12 @@ public class TestBase {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.startMaximized = true;
 
-        if(System.getProperty("remote_driver") != null) {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.browserCapabilities = capabilities;
-            Configuration.remote = System.getProperty("remote_driver");
-        }
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = System.getProperty("remote_driver", "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/");
+
     }
 
     @AfterEach
@@ -31,7 +30,7 @@ public class TestBase {
         attachScreenshot("Last screenshot");
         attachPageSource();
         attachAsText("Browser console logs", getConsoleLogs());
-        if(System.getProperty("video_storage") != null)
+        if (System.getProperty("video_storage") != null)
             attachVideo();
         closeWebDriver();
     }
